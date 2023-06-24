b7d70514014e95fe4840f3716862a40e9591bf1d
Panos Oikonomou <panos.economou.v@gmail.com>
2023-06-24 19:34:58 +0400

    initial commit

---
+++ tests-panos/test.cpp
@@ -0,0 +1,14 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+using namespace cv;
+
+#include <iostream>
+using namespace std;
+
+void main(){
+
+    string path = "";
+
+    return;
+}
