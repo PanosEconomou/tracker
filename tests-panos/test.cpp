62707d90b8f770658aa62dc116b03bbc6fdfd9c1
Panos Oikonomou <panos.economou.v@gmail.com>
2023-06-24 20:33:56 +0400

    live webcam viewer

--- tests-panos/test.cpp
+++ tests-panos/test.cpp
@@ -6,9 +6,36 @@ using namespace cv;
 #include <iostream>
 using namespace std;
 
+
+// View Webcam!
 int main(){
 
-    string path = "";
+    // Start a video capture object on camera 0
+    VideoCapture cap(0);
+
+    // The image object
+    Mat img;
+    Mat imgGrey, imgBlur, imgCanny;
+
+    while (true){
+        //Read the camera frame into the image
+        cap.read(img);
+
+        //Convert the color to grey
+        cvtColor(img,imgGrey,COLOR_BGR2GRAY);
+
+        // Get a blured image too
+        GaussianBlur(img,imgBlur,Size(11,11),3,0);
+
+        //Get the edges
+        Canny(img,imgCanny,50,150);
+
+        // Display the image
+        imshow("Camera of Pano",img);
+        imshow("Grey Camera of Pano",imgCanny);
+
+        waitKey(1);
+    }
 
     return 0;
 }
